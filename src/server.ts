1c2605c34a3c8ee00bbe1f7bff295f39e7268c5a
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-04 22:32:25 +0100

    feat: change subscriptions from ws to sse

--- src/server.ts
+++ src/server.ts
@@ -1,59 +1,44 @@
-import express from "express";
+/**
+ * @file Creates the server instance.
+ */
+
+import express, { Express } from "express";
 import { yogaRequestHandler } from "./yoga.ts";
 import { remixRequestHandler } from "./remix.ts";
-import { WebSocketServer } from "ws";
-import { createServer } from "http";
-import { useServer as wrapServer } from "graphql-ws/lib/use/ws";
+import { createServer, Server } from "http";
 
+/**
+ * Returns the configured express server instance.
+ * This includes the graphql and remix request handlers, and a websocket server for graphql subscriptions.
+ */
 export default function serve() {
   const app = express();
-  const port = process.env.PORT_BACKEND ?? 4000;
 
-  app.use(express.static("public"));
+  const httpServer = createHttpServer(app);
+
+  addRemixRequestHandler(app);
+  addYogaRequestHandler(app);
+
+  start(httpServer);
+}
+
+/**
+ * Adds the yoga GraphQL request handler.
+ */
+function addYogaRequestHandler(app: Express) {
   app.all(yogaRequestHandler.graphqlEndpoint, yogaRequestHandler);
+}
+
+/**
+ * Adds the remix request handler and serves the static frontend javascript that is emitted by remix.
+ */
+function addRemixRequestHandler(app: Express) {
+  app.use(express.static("public"));
   app.all("/", remixRequestHandler);
+}
 
-  const httpServer = createServer(app);
-  const wsServer = new WebSocketServer({
-    server: httpServer,
-    path: yogaRequestHandler.graphqlEndpoint,
-  });
-
-  wrapServer(
-    {
-      // @ts-expect-error should always exist
-      execute: (args) => args.rootValue.execute(args),
-      // @ts-expect-error should always exist
-      subscribe: (args) => args.rootValue.subscribe(args),
-      onSubscribe: async (ctx, msg) => {
-        const { schema, execute, subscribe, contextFactory, parse, validate } =
-          yogaRequestHandler.getEnveloped({
-            ...ctx,
-            req: ctx.extra.request,
-            socket: ctx.extra.socket,
-            params: msg.payload,
-          });
-
-        const args = {
-          schema,
-          operationName: msg.payload.operationName,
-          document: parse(msg.payload.query),
-          variableValues: msg.payload.variables,
-          contextValue: await contextFactory(),
-          rootValue: {
-            execute,
-            subscribe,
-          },
-        };
-
-        const errors = validate(args.schema, args.document);
-        if (errors.length) return errors;
-        return args;
-      },
-    },
-    wsServer,
-  );
-
+function start(httpServer: Server) {
+  const port = process.env.PORT_BACKEND ?? 4000;
   httpServer.listen(port, () => {
     console.info(`Running in ${process.env.NODE_ENV} mode..`);
     console.info(`Listening on ${port}`);
@@ -61,4 +46,8 @@ export default function serve() {
       `Serving GraphQL endpoint at ${yogaRequestHandler.graphqlEndpoint} 😎👌`,
     );
   });
+}
+
+function createHttpServer(app: Express) {
+  return createServer(app);
 }
