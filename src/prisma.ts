b1632ba1fd1c0136ffe00a0d874ca90931949e2a
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-08 21:01:04 +0100

    feat(prisma): add prisma extension

--- src/prisma.ts
+++ src/prisma.ts
@@ -4,4 +4,36 @@
 
 import { PrismaClient } from "@prisma/client";
 
+type QueryArgs = {
+  include?: unknown;
+  select?: unknown;
+};
+
+/**
+ * Returns an extended prisma client that inserts the given query arguments
+ * before the normal arguments that are given when calling the query.
+ * @param prisma the prisma client to extend
+ * @param argsBefore the arguments to add before the normal arguments
+ * @returns the extended prisma client
+ */
+export function extendWithQueryArgsBefore(
+  prisma: PrismaClient,
+  argsBefore: QueryArgs,
+) {
+  return prisma.$extends({
+    query: {
+      $allOperations({ args, query }) {
+        return query({
+          ...argsBefore,
+          ...args,
+        });
+      },
+    },
+  });
+}
+
+export type AnyPrismaClient =
+  | PrismaClient
+  | ReturnType<typeof extendWithQueryArgsBefore>;
+
 export default new PrismaClient({});
