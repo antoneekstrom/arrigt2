c1a543e388215df4905ada8b07dad08cfba68b02
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-20 00:08:46 +0100

    test(unit): add unit tests for events

--- src/model/events.test.ts
+++ src/model/events.test.ts
@@ -1,41 +1,161 @@
-import { describe, expect, it, vi } from "vitest";
-import prisma from "../__mocks__/prisma.ts";
-import { findEventById } from "./events.ts";
+import { describe, expect, it } from "vitest";
+import {
+  inFourWeeks,
+  inOneDay,
+  inOneWeek,
+  oneDayAgo,
+} from "../common/dateTime";
+import {
+  eventSchema,
+  createEventData,
+  eventIsPublishedBeforeOpening,
+  eventOpensBeforeClosing,
+  eventStartsAfterClosing,
+  isEventOpenForRegistering,
+} from "./events";
+import { expectValid } from "../../test/helpers";
 
-vi.mock("../prisma.ts");
-
-describe("find event by id", () => {
-  const events = [
-    {
-      id: "plupp1",
-      title: "apa1",
-      location: "snor",
-      dateTime: new Date(Date.now()),
-    },
-    {
-      id: "plupp2",
-      title: "apa2",
-      location: "snor",
-      dateTime: new Date(Date.now()),
-    },
-    {
-      id: "plupp2",
-      title: "apa2",
-      location: "snor",
-      dateTime: new Date(Date.now()),
-    },
-  ];
-
-  it("throws error when there is more than one id", async () => {
-    prisma.event.findUniqueOrThrow.mockResolvedValue(events[0]!);
-    expect(async () => await findEventById(prisma, "plupp2")).toThrow();
+describe("default event", () => {
+  const now = new Date(Date.now());
+  it("should be valid", () => {
+    expectValid(
+      eventSchema,
+      createEventData("title", "location", oneDayAgo(now)),
+    );
+    expectValid(eventSchema, createEventData("title", "location", now));
+    expectValid(
+      eventSchema,
+      createEventData("title", "location", inFourWeeks(now)),
+    );
   });
-  it("throws error when there is no event with id", async () => {
-    prisma.event.findUniqueOrThrow.mockResolvedValue(events[0]!);
-    expect(async () => await findEventById(prisma, "plupp0")).toThrow();
+});
+
+describe("isEventPublishedValid", () => {
+  const now = new Date(Date.now());
+
+  it("returns true when published and opening at the same time", () => {
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: now,
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: inOneWeek(now),
+      }),
+    ).toBe(true);
   });
-  it("returns the event with the given id", async () => {
-    prisma.event.findUniqueOrThrow.mockResolvedValue(events[0]!);
-    expect(await findEventById(prisma, "plupp1")).toBe(events[0]!);
+
+  it("returns true when published before closing for registration", () => {
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: now,
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: inOneWeek(now),
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: now,
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: oneDayAgo(now),
+      }),
+    ).toBe(false);
+  });
+
+  it("returns true when published before opening for registration", () => {
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: now,
+        opensForRegistrationsAt: inOneDay(now),
+        closesForRegistrationsAt: inFourWeeks(now),
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: now,
+        opensForRegistrationsAt: oneDayAgo(now),
+        closesForRegistrationsAt: inFourWeeks(now),
+      }),
+    ).toBe(false);
+  });
+});
+
+describe("isEventclosesForRegistrationsValid", () => {
+  const now = new Date(Date.now());
+
+  it("returns true when closing for registration after opening for registration", () => {
+    expect(
+      eventOpensBeforeClosing({
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: inOneWeek(now),
+      }),
+    ).toBe(true);
+    expect(
+      eventOpensBeforeClosing({
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: oneDayAgo(now),
+      }),
+    ).toBe(false);
+  });
+});
+
+describe("isEventDateTimeValid", () => {
+  const now = new Date(Date.now());
+
+  it("returns true when event starts after publishing, opening, and closing for registration", () => {
+    expect(
+      eventStartsAfterClosing({
+        dateTime: inOneWeek(now),
+        isPublishedAt: now,
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: inFourWeeks(now),
+      }),
+    ).toBe(true);
+    expect(
+      eventStartsAfterClosing({
+        dateTime: inFourWeeks(now),
+        isPublishedAt: now,
+        opensForRegistrationsAt: now,
+        closesForRegistrationsAt: inOneWeek(now),
+      }),
+    ).toBe(false);
+  });
+});
+
+describe("isEventOpenForRegistration", () => {
+  const now = new Date(Date.now());
+
+  it("returns true when now is before closing", () => {
+    expect(
+      isEventOpenForRegistering(
+        {
+          isPublishedAt: now,
+          opensForRegistrationsAt: now,
+          closesForRegistrationsAt: inFourWeeks(now),
+        },
+        now,
+      ),
+    ).toBe(true);
+    expect(
+      isEventOpenForRegistering(
+        {
+          isPublishedAt: now,
+          opensForRegistrationsAt: now,
+          closesForRegistrationsAt: inFourWeeks(now),
+        },
+        inFourWeeks(now),
+      ),
+    ).toBe(false);
+  });
+
+  it("returns false when now is before opening", () => {
+    expect(
+      isEventOpenForRegistering(
+        {
+          isPublishedAt: now,
+          opensForRegistrationsAt: inOneDay(now),
+          closesForRegistrationsAt: inFourWeeks(now),
+        },
+        now,
+      ),
+    ).toBe(false);
   });
 });
