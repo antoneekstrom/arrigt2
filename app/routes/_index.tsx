56510cc154dca537b21c1164392fc9921849c0e9
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-20 00:12:54 +0100

    feat: add email registration form to frontend

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,14 @@
-import { useEffect, useState } from "react";
-import { useLoaderData } from "@remix-run/react";
-import { ActionFunctionArgs, json } from "@remix-run/node";
+import { useEffect, useId, useState } from "react";
+import { useFetcher, useLoaderData, useSearchParams } from "@remix-run/react";
+import { ActionFunctionArgs, json, redirect } from "@remix-run/node";
 import { query } from "../query";
 import { Event } from "../__generated__/graphql/graphql";
+import {
+  contactInfoSchema,
+  emailRegistrationSchema,
+} from "../../src/model/registrations";
+import { parse } from "@conform-to/zod";
+import { conform, useForm } from "@conform-to/react";
 
 const SUBSCRIPTION_ALL_EVENTS = /* GraphQL */ `
   subscription SubscribeToAllEvents {
@@ -26,11 +32,13 @@ const QUERY_ALL_EVENTS = /* GraphQL */ `
   }
 `;
 
-const MUTATION_EDIT_EVENT = /* GraphQL */ `
-  mutation EditEvent($input: MutationEditEventInput!) {
-    editEvent(input: $input) {
-      id
-      title
+const MUTATION_REGISTER_EMAIL = /* GraphQL */ `
+  mutation RegisterByEmail($input: MutationRegisterToEventByEmailInput!) {
+    registerToEventByEmail(input: $input) {
+      email
+      event {
+        id
+      }
     }
   }
 `;
@@ -59,10 +67,22 @@ export async function loader() {
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
-  const id = formData.get("id");
-  const title = formData.get("title");
-  const { data } = await query(MUTATION_EDIT_EVENT, { input: { id, title } });
-  return json(data as Event);
+
+  const submission = parse(formData, {
+    schema: emailRegistrationSchema
+      .omit({ createdAt: true, personalInfoId: true })
+      .merge(contactInfoSchema.omit({ id: true })),
+  });
+
+  if (!submission.value || submission.intent !== "submit") {
+    return json(submission);
+  }
+
+  const response = await query(MUTATION_REGISTER_EMAIL, {
+    input: submission.value,
+  });
+
+  return redirect(`/?value=${JSON.stringify(response)}`);
 }
 
 export default function Events() {
@@ -82,7 +102,72 @@ export default function Events() {
   );
 }
 
-const EventListItem = (props: Partial<Event>) => {
+const EmailRegistration = (props: Pick<Event, "id">) => {
+  const fetcher = useFetcher<typeof action>();
+  const lastSubmission = fetcher.data;
+  const id = useId();
+  const [form, fields] = useForm({
+    lastSubmission,
+    shouldValidate: "onBlur",
+    id,
+    onValidate({ formData }) {
+      return parse(formData, {
+        schema: emailRegistrationSchema
+          .omit({ createdAt: true, personalInfoId: true })
+          .merge(contactInfoSchema.omit({ id: true })),
+      });
+    },
+  });
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  if (
+    searchParams.has("value") &&
+    JSON.parse(searchParams.get("value")!).data.registerToEventByEmail.event
+      .id === props.id
+  ) {
+    return (
+      <div>
+        <h1>
+          Registered with email{" "}
+          {
+            JSON.parse(searchParams.get("value")!).data.registerToEventByEmail
+              .email
+          }
+        </h1>
+        <button onClick={() => setSearchParams(new URLSearchParams())}>
+          ok thank
+        </button>
+        <p>{searchParams.get("value")}</p>
+      </div>
+    );
+  }
+
+  return (
+    <fetcher.Form method="post" {...form.props}>
+      <input type="hidden" name="eventId" value={props.id} />
+      <div>{lastSubmission?.intent}</div>
+      <div>
+        <label htmlFor={fields.email.id}>Email</label>
+        <input {...conform.input(fields.email, { type: "email" })} />
+        <div id={fields.email.errorId}>{fields.email.errors}</div>
+      </div>
+      <div>
+        <label htmlFor={fields.firstName.id}>First Name</label>
+        <input {...conform.input(fields.firstName, { type: "text" })} />
+        <div id={fields.firstName.errorId}>{fields.firstName.errors}</div>
+      </div>
+      <div>
+        <label htmlFor={fields.lastName.id}>Last Name</label>
+        <input {...conform.input(fields.lastName, { type: "text" })} />
+        <div id={fields.lastName.errorId}>{fields.lastName.errors}</div>
+      </div>
+      <button type="submit">Register</button>
+    </fetcher.Form>
+  );
+};
+
+const EventListItem = (props: DeepPartial<Event>) => {
   return (
     <li>
       <article>
@@ -90,12 +175,17 @@ const EventListItem = (props: Partial<Event>) => {
         <p>{props.dateTime?.toString()}</p>
         <p>{props.location}</p>
         <p>{props.id}</p>
+        {props.id && <EmailRegistration id={props.id} />}
       </article>
     </li>
   );
 };
 
-function useAllEventsSubscription(initial: Partial<Event>[] = []) {
+type DeepPartial<T> = {
+  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
+};
+
+function useAllEventsSubscription(initial: DeepPartial<Event>[] = []) {
   const [events, setEvents] = useState(initial);
   useEffect(() => {
     const url = new URL("http://localhost:4000/graphql");
