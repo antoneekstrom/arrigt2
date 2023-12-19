f0c87fcd5b74c34e0c4dba1faf12ad21817d57d5
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-20 00:03:32 +0100

    build(codegen): add test directory to codegen

--- graphql.config.ts
+++ graphql.config.ts
@@ -2,5 +2,5 @@ import type { IGraphQLConfig } from "graphql-config";
 
 export default {
   schema: process.env.CI === "true" ? "schema.graphql" : "http://localhost:4000/graphql",
-  documents: "./app/**/*.{ts,tsx}"
+  documents: ["./app/**/*.{ts,tsx}", "./test/**/*.{ts,tsx}"]
 } satisfies IGraphQLConfig;
