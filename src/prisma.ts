aaa0dd068fb054b04c870168129dfdcdee596535
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-20 23:16:49 +0100

    feat: add pubsub extension for prisma client

--- src/prisma.ts
+++ src/prisma.ts
@@ -2,35 +2,36 @@
  * @file Configures and exports the prisma client.
  */
 
-import { PrismaClient } from "@prisma/client";
-
-type ResolverQueryArgs = {
-  include?: unknown;
-  select?: unknown;
-};
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DynamicClientExtensionThis } from "@prisma/client/runtime/library";
+import { PubSub } from "./yoga";
 
 type AnyPrismaClient =
   | PrismaClient
-  | ReturnType<typeof extendWithResolverQueryArgs>;
+  | DynamicClientExtensionThis<
+      Prisma.TypeMap,
+      Prisma.TypeMapCb,
+      Record<string, unknown>
+    >;
 
 const prisma = new PrismaClient({});
 
 /**
- * Returns an extended prisma client that inserts the given query arguments
- * before the normal arguments that are given when calling the query.
- * @param prisma the prisma client to extend
- * @param argsBefore the arguments to add before the normal arguments
+ * Returns a prisma client extension that injects the given query arguments along with
+ * the normal arguments that are given when calling the query.
+ * @param extra the arguments to add
  * @returns the extended prisma client
  */
-export function extendWithResolverQueryArgs(
-  prisma: PrismaClient,
-  argsBefore: ResolverQueryArgs,
-) {
-  return prisma.$extends({
+export function injectQueryArgsExtension(extra: {
+  include?: unknown;
+  select?: unknown;
+}) {
+  return Prisma.defineExtension({
+    name: injectQueryArgsExtension.name,
     query: {
       $allOperations({ args, query }) {
         return query({
-          ...argsBefore,
+          ...extra,
           ...args,
         });
       },
@@ -38,22 +39,64 @@ export function extendWithResolverQueryArgs(
   });
 }
 
+/**
+ *
+ * @param pubsub
+ * @returns
+ */
+export function pubsubExtension(pubsub: PubSub) {
+  return Prisma.defineExtension({
+    name: pubsubExtension.name,
+    query: {
+      $allModels: {},
+      $allOperations({ args, query, operation, model }) {
+        if (
+          !operation.includes("create") &&
+          !operation.includes("update") &&
+          !operation.includes("delete")
+        ) {
+          console.log("Skipping publish event: ", operation);
+          return query(args);
+        }
+
+        if (model) {
+          console.log(`Publishing event: ${model}/${operation}`);
+          pubsub.publish(model, operation);
+        }
+
+        return query(args);
+      },
+    },
+  });
+}
+
+type DelegateConstructor<T extends PrismaDelegate> = new (
+  prisma: AnyPrismaClient,
+) => T;
+
+/**
+ *
+ */
 export class PrismaDelegate {
   constructor(protected readonly prisma: AnyPrismaClient) {}
 
-  static fromSingleton<T extends PrismaDelegate>(delegate: {
-    new (prisma: AnyPrismaClient): T;
-  }): T {
+  static fromSingleton<T extends PrismaDelegate>(
+    delegate: DelegateConstructor<T>,
+  ): T {
     return new delegate(prisma);
   }
 
   static fromResolverArgs<T extends PrismaDelegate>(
-    delegate: {
-      new (prisma: AnyPrismaClient): T;
-    },
-    args: ResolverQueryArgs,
+    delegate: DelegateConstructor<T>,
+    args: Parameters<typeof injectQueryArgsExtension>[0],
   ): T {
-    return new delegate(extendWithResolverQueryArgs(prisma, args));
+    const extendedClient = prisma.$extends(injectQueryArgsExtension(args));
+    return new delegate(extendedClient);
+  }
+
+  injectQueryArgs(args: Parameters<typeof injectQueryArgsExtension>[0]) {
+    this.prisma.$extends(injectQueryArgsExtension(args));
+    return this;
   }
 }
 
