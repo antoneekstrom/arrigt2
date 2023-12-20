d59ed19aa68c5f066d63106b73527d6c166c4157
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-20 23:17:24 +0100

    feat: add models to yoga context

--- src/yoga.ts
+++ src/yoga.ts
@@ -3,21 +3,34 @@
  */
 
 import { createPubSub, createYoga, YogaInitialContext } from "graphql-yoga";
+import prisma, { pubsubExtension } from "./prisma";
 import schema from "./schema";
+import { Events } from "./model/events";
+import { Registrations } from "./model/registrations";
 
-const initialContext = {
-  // Keeps track of GraphQL subscriptions
-  pubsub: createPubSub(),
+export type PubSub = ReturnType<typeof createPubSub>;
+
+const createInitialContext = () => {
+  const pubsub = createPubSub<{
+    [model: string]: [string];
+  }>();
+  return {
+    // Keeps track of GraphQL subscriptions
+    pubsub,
+    events: new Events(prisma.$extends(pubsubExtension(pubsub))),
+    registrations: new Registrations(prisma.$extends(pubsubExtension(pubsub))),
+  };
 };
 
 export const yogaRequestHandler = createYoga({
   schema,
   graphiql: process.env.NODE_ENV === "development",
   graphqlEndpoint: process.env.ENDPOINT_GRAPHQL,
-  context: initialContext,
+  context: createInitialContext(),
 });
 
 /**
  * Describes the GraphQL context.
  */
-export type YogaContext = YogaInitialContext & typeof initialContext;
+export type YogaContext = YogaInitialContext &
+  ReturnType<typeof createInitialContext>;
