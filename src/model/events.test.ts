c9e89fd679f6a08d959f1b05ae60b210adae49bc
Anton Ekström <anton.e.ekstrom@gmail.com>
2023-12-25 13:59:58 +0100

    test: reimplement unit tests for events

--- src/model/events.test.ts
+++ src/model/events.test.ts
@@ -1,161 +1,164 @@
-import { describe, expect, it } from "vitest";
+import { expect } from "vitest";
+import { describe, it } from "vitest";
 import {
-  inFourWeeks,
-  inOneDay,
-  inOneWeek,
-  oneDayAgo,
-} from "../common/dateTime";
-import {
-  eventSchema,
-  createEventData,
+  assertEventIsValid,
+  defaultEventData,
   eventIsPublishedBeforeOpening,
-  eventOpensBeforeClosing,
-  eventStartsAfterClosing,
-  isEventOpenForRegistering,
 } from "./events";
-import { expectValid } from "../../test/helpers";
+import { inOneDay, inOneWeek, oneDayAgo, oneWeekAgo } from "../common/dateTime";
 
-describe("default event", () => {
-  const now = new Date(Date.now());
+describe("Default event data", () => {
   it("should be valid", () => {
-    expectValid(
-      eventSchema,
-      createEventData("title", "location", oneDayAgo(now)),
-    );
-    expectValid(eventSchema, createEventData("title", "location", now));
-    expectValid(
-      eventSchema,
-      createEventData("title", "location", inFourWeeks(now)),
-    );
+    const valid = defaultEventData({
+      title: "title",
+      location: "location",
+      dateTime: inOneWeek(),
+    });
+    const invalidDateTime: typeof valid = {
+      ...valid,
+      dateTime: oneWeekAgo(),
+    };
+    const missingSomeData: Partial<typeof valid> = {
+      ...valid,
+    };
+    delete missingSomeData.opensForRegistrationsAt;
+
+    expect(() => assertEventIsValid(valid)).not.toThrowError();
+    expect(() => assertEventIsValid(invalidDateTime)).toThrowError();
+    expect(() =>
+      assertEventIsValid(missingSomeData as typeof valid),
+    ).toThrowError();
   });
 });
 
-describe("isEventPublishedValid", () => {
-  const now = new Date(Date.now());
+describe("Event date constraints", () => {
+  const today = new Date(Date.now());
+  const yesterday = oneDayAgo(today);
+  const tomorrow = inOneDay(today);
 
-  it("returns true when published and opening at the same time", () => {
+  it("Should be published before opening.", () => {
     expect(
       eventIsPublishedBeforeOpening({
-        isPublishedAt: now,
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: inOneWeek(now),
-      }),
-    ).toBe(true);
-  });
-
-  it("returns true when published before closing for registration", () => {
-    expect(
-      eventIsPublishedBeforeOpening({
-        isPublishedAt: now,
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: inOneWeek(now),
+        isPublishedAt: yesterday,
+        opensForRegistrationsAt: tomorrow,
       }),
     ).toBe(true);
     expect(
       eventIsPublishedBeforeOpening({
-        isPublishedAt: now,
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: oneDayAgo(now),
-      }),
-    ).toBe(false);
-  });
-
-  it("returns true when published before opening for registration", () => {
-    expect(
-      eventIsPublishedBeforeOpening({
-        isPublishedAt: now,
-        opensForRegistrationsAt: inOneDay(now),
-        closesForRegistrationsAt: inFourWeeks(now),
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: tomorrow,
       }),
     ).toBe(true);
     expect(
       eventIsPublishedBeforeOpening({
-        isPublishedAt: now,
-        opensForRegistrationsAt: oneDayAgo(now),
-        closesForRegistrationsAt: inFourWeeks(now),
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: today,
       }),
     ).toBe(false);
   });
-});
 
-describe("isEventclosesForRegistrationsValid", () => {
-  const now = new Date(Date.now());
-
-  it("returns true when closing for registration after opening for registration", () => {
+  it("Should be published before closing.", () => {
     expect(
-      eventOpensBeforeClosing({
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: inOneWeek(now),
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: yesterday,
+        opensForRegistrationsAt: tomorrow,
       }),
     ).toBe(true);
     expect(
-      eventOpensBeforeClosing({
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: oneDayAgo(now),
-      }),
-    ).toBe(false);
-  });
-});
-
-describe("isEventDateTimeValid", () => {
-  const now = new Date(Date.now());
-
-  it("returns true when event starts after publishing, opening, and closing for registration", () => {
-    expect(
-      eventStartsAfterClosing({
-        dateTime: inOneWeek(now),
-        isPublishedAt: now,
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: inFourWeeks(now),
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: tomorrow,
       }),
     ).toBe(true);
     expect(
-      eventStartsAfterClosing({
-        dateTime: inFourWeeks(now),
-        isPublishedAt: now,
-        opensForRegistrationsAt: now,
-        closesForRegistrationsAt: inOneWeek(now),
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: today,
       }),
     ).toBe(false);
   });
-});
 
-describe("isEventOpenForRegistration", () => {
-  const now = new Date(Date.now());
-
-  it("returns true when now is before closing", () => {
+  it("Should be published before starting.", () => {
     expect(
-      isEventOpenForRegistering(
-        {
-          isPublishedAt: now,
-          opensForRegistrationsAt: now,
-          closesForRegistrationsAt: inFourWeeks(now),
-        },
-        now,
-      ),
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: yesterday,
+        opensForRegistrationsAt: tomorrow,
+      }),
     ).toBe(true);
     expect(
-      isEventOpenForRegistering(
-        {
-          isPublishedAt: now,
-          opensForRegistrationsAt: now,
-          closesForRegistrationsAt: inFourWeeks(now),
-        },
-        inFourWeeks(now),
-      ),
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: today,
+      }),
     ).toBe(false);
   });
 
-  it("returns false when now is before opening", () => {
+  it("Should be opening before closing.", () => {
     expect(
-      isEventOpenForRegistering(
-        {
-          isPublishedAt: now,
-          opensForRegistrationsAt: inOneDay(now),
-          closesForRegistrationsAt: inFourWeeks(now),
-        },
-        now,
-      ),
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: yesterday,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: today,
+      }),
+    ).toBe(false);
+  });
+
+  it("Should be opening before starting.", () => {
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: yesterday,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: today,
+      }),
+    ).toBe(false);
+  });
+
+  it("Should be starting before closing.", () => {
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: yesterday,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: tomorrow,
+      }),
+    ).toBe(true);
+    expect(
+      eventIsPublishedBeforeOpening({
+        isPublishedAt: tomorrow,
+        opensForRegistrationsAt: today,
+      }),
     ).toBe(false);
   });
 });
