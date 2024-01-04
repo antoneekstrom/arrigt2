33e05480a48637cb3b903dece38bc911cf8f71f2
Anton Ekström <anton.e.ekstrom@gmail.com>
2024-01-04 19:06:46 +0100

    build(codegen): change to always use local schema

--- graphql.config.ts
+++ graphql.config.ts
@@ -1,6 +1,6 @@
 import type { IGraphQLConfig } from "graphql-config";
 
 export default {
-  schema: process.env.CI === "true" ? "schema.graphql" : "http://localhost:4000/graphql",
+  schema: "schema.graphql",
   documents: ["./app/**/*.{ts,tsx}", "./test/**/*.{ts,tsx}"]
 } satisfies IGraphQLConfig;
