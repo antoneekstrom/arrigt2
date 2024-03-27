1b5104b51e1fae2f587c00b973ed4d244d403508
Anton Ekström <anton.e.ekstrom@gmail.com>
2024-03-27 19:00:38 +0100

    refactor: update prisma extension imports

--- app/helpers/db.server.ts
+++ app/helpers/db.server.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { EventsModel } from "../../src/model/events";
-import { RegistrationsModel } from "../../src/model/registrations";
+import { EventsModel } from "../../src/prisma/EventsModel";
+import { RegistrationsModel } from "../../src/prisma/RegistrationsModel";
 
 /**
  * Exports the prisma client in a server-only remix module so that it does not leak into client browser or apply any side-effects multiple times.
